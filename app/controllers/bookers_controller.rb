f4f5a243c968603dc4904fe2eaf8dfabdef2e16d
maemori <hei5342hei@gmail.com>
2018-06-14 05:20:45 +0000

    start 4-8

---
+++ app/controllers/bookers_controller.rb
@@ -0,0 +1,55 @@
+class BookersController < ApplicationController
+  def top
+  end
+
+  def index
+      @bookers = Booker.all
+      @booker = Booker.new
+  end
+
+  def show
+      @booker = Booker.find(params[:id])
+  end
+
+  def create
+      @book = Booker.new(booker_params)
+       if @book.save
+          flash[:notice] = "Book was successfully created."
+          redirect_to booker_path(booker.id)
+       else
+          @bookers = Booker.all
+          @booker = @book
+          # binding pry
+          render action: :index
+        end
+  end
+
+  def new
+      #空のモデル渡す
+      @booker = Booker.new
+  end
+
+  def edit
+      @booker = Booker.find(params[:id])
+  end
+
+  def update
+      booker = Booker.find(params[:id])
+      if booker.update(booker_params)
+         flash[:notice] = "Book was successfully updated."
+         redirect_to booker_path(booker.id)
+      else
+        render action: :show
+      end
+  end
+
+  def destroy
+      booker = Booker.find(params[:id])
+      booker.destroy
+      redirect_to bookers_index_path
+  end
+
+  def booker_params
+      params.require(:booker).permit(:title, :body)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+	get 'bookers/new'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  	# get 'bookers/show' => 'bookers#show'
+  	get 'bookers/top' => 'bookers#top'
+  	post '/bookers' => 'bookers#create'
+  	get '/bookers/index' => 'bookers#index'
+  	get '/bookers/:id' => 'bookers#show', as: 'booker'
+  	patch '/bookers/:id' => 'bookers#update', as: 'update_booker'
+  	get '/bookers/:id/edit' => 'bookers#edit', as: 'edit_booker'
+  	delete '/bookers/:id' => 'bookers#destroy', as: 'destroy_booker'
+end
